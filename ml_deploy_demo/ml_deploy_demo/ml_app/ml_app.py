6781bb0b3eb2ad971f405d84268a0abf3aad1bee
Donghun Lee <donghun.lee@paige.ai>
2020-01-05 17:35:15 -0500

    Add flask app.

---
+++ ml_deploy_demo/ml_deploy_demo/ml_app/ml_app.py
@@ -0,0 +1,24 @@
+import logging
+
+from flask import Blueprint, request, jsonify
+
+logger =  logging.getLogger(__name__)
+
+ml_app = Blueprint('ml_app', __name__)
+
+
+@ml_app.route("/predict", methods=["GET", "POST"])
+def predict():
+    """
+    """
+    if request.method == "POST":
+        data = request.get_json()
+        logger.debug(f"Input to predict/: {data}")
+        return jsonify({"output": "hello post" })
+
+    if request.method == "GET":
+        data = request.get_json()
+        logger.debug(f"Input to predict/: {data}")
+        return jsonify({"output": "hello get" })
+
+
