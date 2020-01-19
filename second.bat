f26fca86316680657c107cb5e2fd52704b3bb544
Ignacio <iebecker@users.noreply.github.com>
2020-01-18 20:34:25 -0500

    Add files via upload

---
+++ second.bat
@@ -0,0 +1,6 @@
+pushd "%~dp0"
+dir /b %SystemRoot%\servicing\Packages\*containers*.mum >containers.txt
+for /f %%i in ('findstr /i . containers.txt 2^>nul') do dism /online /norestart /add-package:"%SystemRoot%\servicing\Packages\%%i"
+del containers.txt
+Dism /online /enable-feature /featurename:Containers -All /LimitAccess /ALL
+pause
